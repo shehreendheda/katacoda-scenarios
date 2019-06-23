58428e2c13573e52c15f883cfc7a9e6302f29ac8
shehreendheda <shehreendheda@users.noreply.github.com>
2019-06-23 12:25:03 -0700

    Update tasks.py

--- udf_python/assets/tasks.py
+++ udf_python/assets/tasks.py
@@ -14,7 +14,7 @@ def step2task(mysumcube):
         print("Please try again!")
 
 def step3task(mysumsq3):
-    if mysumsq3 == "The sum of sqaures is a multiple of 3.":
+    if mysumsq3 == "The sum of squares is a multiple of 3.":
         print("Correct!")
     else:
         print("Please try again!")
