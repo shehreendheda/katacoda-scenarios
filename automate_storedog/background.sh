08b6024f127ca9c9b1b95721e29b42eb5aac445b
sdheda <shehreendheda@users.noreply.github.com>
2019-11-06 07:47:27 -0800

    removed docker-compose up

--- automate_storedog/background.sh
+++ automate_storedog/background.sh
@@ -4,10 +4,3 @@ git clone https://github.com/burningion/ecommerce-observability /create-slo
 cd /create-slo
 git checkout instrumented-fixed
 docker-compose pull
-
-export POSTGRES_USER=postgres
-export POSTGRES_PASSWORD=postgres
-cd /create-slo
-docker-compose up -d
-
-./gor --input-file-loop --input-file requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
