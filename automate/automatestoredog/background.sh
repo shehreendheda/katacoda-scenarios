6b4a54b26bfa29711946be55f306648d823de9db
sdheda <shehreendheda@users.noreply.github.com>
2019-11-11 14:46:10 -0800

    added cd to foreground

--- automate/automatestoredog/background.sh
+++ automate/automatestoredog/background.sh
@@ -8,4 +8,8 @@ docker-compose pull
 #while [ ! -f /root/.kcrc ]; do sleep 1; done
 #source /root/.kcrc
 
+#export POSTGRES_USER=postgres
+#export POSTGRES_PASSWORD=postgres
+#docker-compose up -d
+
 ./gor --input-file-loop --input-file requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
--- automate/automatestoredog/foreground.sh
+++ automate/automatestoredog/foreground.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-sleep 10
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
+cd /create-slo
+clear
+sleep 10
 docker-compose up -d
 clear
 prepping-the-environment
