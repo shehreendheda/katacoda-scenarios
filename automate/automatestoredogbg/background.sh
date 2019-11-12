3f7d752abfc33fa63c8d5a49dd0c23e69eecf182
sdheda <shehreendheda@users.noreply.github.com>
2019-11-12 11:12:27 -0800

    created automatestoredog for foreground and background

---
+++ automate/automatestoredogbg/background.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir /create-slo
+git clone https://github.com/burningion/ecommerce-observability /create-slo
+cd /create-slo
+git checkout instrumented-fixed
+docker-compose pull
+
+while [ ! -f /root/.kcrc ]; do sleep 1; done
+source /root/.kcrc
+
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=postgres
+docker-compose up -d
+
+./gor --input-file-loop --input-file requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
