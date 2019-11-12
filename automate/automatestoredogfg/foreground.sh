6702335cd8b35e00e8af358b277ab011db9d4311
sdheda <shehreendheda@users.noreply.github.com>
2019-11-12 11:56:40 -0800

    replaced sleep with while loop

--- automate/automatestoredogfg/foreground.sh
+++ automate/automatestoredogfg/foreground.sh
@@ -2,7 +2,8 @@
 
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
-sleep 15
+#sleep 15
+while [ ! -f /create-slo/docker-compose.yml ]; do sleep 1; done
 cd /create-slo
 docker-compose up -d
 clear
