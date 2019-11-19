155c63301de45f037bbfa3bfad62becf12aa2e61
sdheda <shehreendheda@users.noreply.github.com>
2019-11-19 08:12:16 -0800

    moved check-file code into foreground

--- slo/actionslos/foreground.sh
+++ slo/actionslos/foreground.sh
@@ -3,8 +3,7 @@
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
 clear
-sleep 2
-check-file
+while [ ! -f /app-files/docker-compose.yml ]; do sleep 2; done
 cd /app-files
 clear
 docker-compose up -d
