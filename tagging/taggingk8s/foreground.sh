646d1938c319f15bf311c989d2dacd4f72c98239
sdheda <shehreendheda@users.noreply.github.com>
2019-12-12 13:46:19 -0800

    updated code for deployment

--- tagging/taggingk8s/foreground.sh
+++ tagging/taggingk8s/foreground.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-
-apt install datamash
-
+while [ ! -f /app-files/lotsofpods.yaml ]; do sleep 1; done
+while [ ! -f /app-files/datadog.yaml ]; do sleep 1; done
+clear
 kubectl create secret generic datadog-api --from-literal=token=$DD_API_KEY
 kubectl apply -f taggingk8s/lotsofpods.yaml
 kubectl apply -f taggingk8s/datadog.yaml
