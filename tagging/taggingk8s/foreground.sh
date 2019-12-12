d22ea5481910a011cb4f17418a329b1e2311d8df
sdheda <shehreendheda@users.noreply.github.com>
2019-12-12 13:53:04 -0800

    updated file dir

--- tagging/taggingk8s/foreground.sh
+++ tagging/taggingk8s/foreground.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-while [ ! -f /app-files/lotsofpods.yaml ]; do sleep 1; done
-while [ ! -f /app-files/datadog.yaml ]; do sleep 1; done
+while [ ! -f /taggingk8s/lotsofpods.yaml ]; do sleep 1; done
+while [ ! -f /taggingk8s/datadog.yaml ]; do sleep 1; done
 clear
 kubectl create secret generic datadog-api --from-literal=token=$DD_API_KEY
 kubectl apply -f taggingk8s/lotsofpods.yaml
