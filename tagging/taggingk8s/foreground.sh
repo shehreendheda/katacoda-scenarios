ca7d45093ce3a4d35c4c09563b98c417e337ff83
sdheda <shehreendheda@users.noreply.github.com>
2019-12-12 13:40:08 -0800

    added code for deployment

--- tagging/taggingk8s/foreground.sh
+++ tagging/taggingk8s/foreground.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+
+apt install datamash
+
+kubectl create secret generic datadog-api --from-literal=token=$DD_API_KEY
+kubectl apply -f taggingk8s/lotsofpods.yaml
+kubectl apply -f taggingk8s/datadog.yaml
