0a9381bac8b12e4852d4f288f0a4a044ed6dee11
sdheda <shehreendheda@users.noreply.github.com>
2020-04-01 14:28:16 -0700

    updated with new repo,branch and removed docker-compose up

--- apmintro/instrumentapp2/foreground.sh
+++ apmintro/instrumentapp2/foreground.sh
@@ -5,7 +5,7 @@ export POSTGRES_PASSWORD=postgres
 clear
 while [ ! -f /root/app-files/docker-compose.yml ]; do sleep 2; done
 cd /root/app-files
-clear
-docker-compose up -d
+#clear
+#docker-compose up -d
 clear
 prep-environment
