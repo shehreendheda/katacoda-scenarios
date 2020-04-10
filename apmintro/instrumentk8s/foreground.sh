54425fc958a855873043835e44c8cf2d0ef0f46a
sdheda <shehreendheda@users.noreply.github.com>
2020-04-10 11:13:47 -0700

    testing export for katacoda_host and sub_domain

--- apmintro/instrumentk8s/foreground.sh
+++ apmintro/instrumentk8s/foreground.sh
@@ -7,6 +7,8 @@ sleep 0.3
 
 kubectl create secret generic datadog-secret --from-literal=api-key=$DD_API_KEY
 clear
-
-https://[[HOST_SUBDOMAIN]]-30001-[[KATACODA_HOST]].environments.katacoda.com/
+export host_name=[[KATACODA_HOST]]
+export sub_domain=[[HOST_SUBDOMAIN]]
+echo $host_name
+echo $sub_domain
 prepenvironment
