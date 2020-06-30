cb87d59754269e54ca38cdc5977d11758b18c6c8
sdheda <shehreendheda@users.noreply.github.com>
2020-06-30 15:25:49 -0700

    updated bg and fg to use ecommerce-workshop repo

--- apmintro/instrumentapp2/foreground.sh
+++ apmintro/instrumentapp2/foreground.sh
@@ -3,9 +3,9 @@
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
 clear
-while [ ! -f /root/app-files/docker-compose.yml ]; do sleep 2; done
-cd /root/app-files
+while [ ! -f /root/app-files/docker-compose-files/docker-compose-broken-no-apm-instrumentation.yml ]; do sleep 2; done
+cd /root/app-files/docker-compose-files
 clear
-docker-compose up -d
+docker-compose -f docker-compose-broken-no-apm-instrumentation.yml up -d
 clear
 prep-environment
