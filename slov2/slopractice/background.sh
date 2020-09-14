402e57d345647f95f2e5f0ba4035fb5295ff4c5d
sdheda <shehreendheda@users.noreply.github.com>
2020-09-14 16:04:22 -0700

    moving app_ready alias to bg

--- slov2/slopractice/background.sh
+++ slov2/slopractice/background.sh
@@ -5,4 +5,8 @@ git clone https://github.com/DataDog/ecommerce-workshop /root/app-files
 cd /root/app-files
 docker-compose pull
 
+{
+  echo "alias app_ready=\"printf '\n\nThe app has restarted! You can continue.\n\n'\""
+} >> ~/.bashrc
+
 ./gor --input-file-loop --input-file requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
--- slov2/slopractice/brokenapp.sh
+++ slov2/slopractice/brokenapp.sh
@@ -4,6 +4,4 @@ docker-compose -f docker-compose-fixed-instrumented.yml stop
 clear
 docker-compose -f docker-compose-broken-instrumented.yml up -d
 clear
-alias app_ready="printf '\n\nThe app has restarted! You can continue.\n\n'"
-clear
 app_ready
