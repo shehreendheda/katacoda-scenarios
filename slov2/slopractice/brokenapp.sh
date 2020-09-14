3341324670e135a080626131f5905e344aae7f8d
sdheda <shehreendheda@users.noreply.github.com>
2020-09-14 14:05:02 -0700

    added clears and printf message

--- slov2/slopractice/brokenapp.sh
+++ slov2/slopractice/brokenapp.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 docker container kill $(docker ps -q)
+clear
 docker-compose -f docker-compose-broken-instrumented.yml up -d
+clear
+printf '\n\nThe app has restarted! You can continue.\n\n'
