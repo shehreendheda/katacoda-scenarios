e83d45283d217e4f49497a3c668dc8246e822d74
sdheda <shehreendheda@users.noreply.github.com>
2020-09-14 09:24:03 -0700

    created slopractice index, fg, bg, steps for slov2

---
+++ slov2/foreground.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=postgres
+clear
+while [ ! -f /root/app-files/docker-compose-files/docker-compose-fixed-instrumented.yml ]; do sleep 2; done
+cd /root/app-files/docker-compose-files
+clear
+docker-compose -f docker-compose-fixed-instrumented.yml up -d
+clear
+prep-environment
