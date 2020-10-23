73b4cd3b7d935b9cb9e865547084d54e0ec97441
sdheda <shehreendheda@users.noreply.github.com>
2020-10-23 14:43:07 -0700

    updated commands so that editor dir is /root/lab-files

--- logsintrov2/managelogs/foreground.sh
+++ logsintrov2/managelogs/foreground.sh
@@ -1,17 +1,20 @@
 # docker pull datadog/agent:6.11.1
 # docker pull redis:5.0.5
 # docker pull nginx:1.16.0
+
+git clone https://github.com/DataDog/TrainingEnvironment.git
+cp -r TrainingEnvironment/logsintro/* lab-files
+rm -rf TrainingEnvironment
+cd /root/lab-files
+
 cat > apikey.env <<EOL
 ---
 DD_API_KEY=${DD_API_KEY}
 EOL
 
-git clone https://github.com/DataDog/TrainingEnvironment.git
-cp -r TrainingEnvironment/logsintro/* .
-rm -rf TrainingEnvironment
-sed -i 's|vagrant|var/log|g' /root/main.py
-sed -i "s|write_text_log('.|write_text_log('/var/log|g" /root/main.py
-sed -i "s|write_json_log('.|write_json_log('/var/log|g" /root/main.py
+sed -i 's|vagrant|var/log|g' /root/lab-files/main.py
+sed -i "s|write_text_log('.|write_text_log('/var/log|g" /root/lab-files/main.py
+sed -i "s|write_json_log('.|write_json_log('/var/log|g" /root/lab-files/main.py
 
 rm install.sh
 rm provision.sh
