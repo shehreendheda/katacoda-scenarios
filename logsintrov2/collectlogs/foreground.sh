fcd97cefb49ba9fe371d9c42fd4a0cb9536a0a46
sdheda <shehreendheda@users.noreply.github.com>
2020-10-23 14:23:11 -0700

    moved EOL to after cd /root/lab-files

--- logsintrov2/collectlogs/foreground.sh
+++ logsintrov2/collectlogs/foreground.sh
@@ -1,15 +1,16 @@
 # docker pull datadog/agent:6.11.1
 # docker pull redis:5.0.5
 # docker pull nginx:1.16.0
-cat > apikey.env <<EOL
----
-DD_API_KEY=${DD_API_KEY}
-EOL
 
 git clone https://github.com/DataDog/TrainingEnvironment.git
 cp -r TrainingEnvironment/logsintro/* lab-files
 rm -rf TrainingEnvironment
 cd /root/lab-files
+
+cat > apikey.env <<EOL
+---
+DD_API_KEY=${DD_API_KEY}
+EOL
 
 sed -i '12 i \ \ \ \ \ \ - DD_API_KEY' docker-compose.yml 
 sed -i '12 i \ \ \ \ \ \ - DD_LOGS_ENABLED=true' docker-compose.yml 
