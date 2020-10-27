c0735efd04db8a43db1f0332d5a0fc953ba13f12
sdheda <shehreendheda@users.noreply.github.com>
2020-10-27 14:29:00 -0700

    testing folder in assets

--- automate/storedogimagetest/foreground.sh
+++ automate/storedogimagetest/foreground.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export POSTGRES_USER=postgres
-export POSTGRES_PASSWORD=postgres
-clear
-while [ ! -f /root/app-files/docker-compose.yml ]; do sleep 2; done
-cd /root/app-files
-clear
-docker-compose up -d
-clear
-prep-environment#!/bin/bash
+#export POSTGRES_USER=postgres
+#export POSTGRES_PASSWORD=postgres
+#clear
+#while [ ! -f /root/app-files/docker-compose.yml ]; do sleep 2; done
+#cd /root/app-files
+#clear
+#docker-compose up -d
+#clear
+#prep-environment#!/bin/bash
