0015667d97cd044182e28883a446d689ceb0ec4f
sdheda <shehreendheda@users.noreply.github.com>
2020-10-28 14:58:58 -0700

    commented sed in fg, update storefrontend dir for files

--- logsintrov2/collectlogsapp/foreground.sh
+++ logsintrov2/collectlogsapp/foreground.sh
@@ -5,7 +5,7 @@ export POSTGRES_PASSWORD=postgres
 clear
 while [ ! -f /root/app-files/docker-compose-files/docker-compose-fixed-instrumented.yml ]; do sleep 2; done
 cd /root/app-files/docker-compose-files
-sed -i 's/ads-service/advertisements-service/' docker-compose-fixed-instrumented.yml
+#sed -i 's/ads-service/advertisements-service/' docker-compose-fixed-instrumented.yml
 clear
 docker-compose -f docker-compose-fixed-instrumented.yml up -d
 clear
