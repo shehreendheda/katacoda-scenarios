0e74aa1e6b4f147ffd4143b522e2069bdef1935d
sdheda <shehreendheda@users.noreply.github.com>
2020-10-28 16:19:51 -0700

    added .yml to mv command in fg, corrected label syntax in yml

--- logsintrov2/collectlogsapp/foreground.sh
+++ logsintrov2/collectlogsapp/foreground.sh
@@ -5,7 +5,7 @@ export POSTGRES_PASSWORD=postgres
 clear
 while [ ! -f /root/app-files/docker-compose-files/docker-compose-broken-no-apm-instrumentation.yml ]; do sleep 2; done
 cd /root/app-files
-mv /root/docker-compose-no-instrumentation /root/app-files/docker-compose-no-instrumentation
+mv /root/docker-compose-no-instrumentation.yml /root/app-files/docker-compose-no-instrumentation.yml
 #mv docker-compose-broken-no-apm-instrumentation.yml docker-compose-broken-no-instrumentation.yml
 #rm docker-compose-broken-no-apm-instrumentation.yml
 #sed -i 's/ads-service/advertisements-service/' docker-compose-fixed-instrumented.yml
