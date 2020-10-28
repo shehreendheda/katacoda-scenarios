eeb654d192c526f09e467d4b2caee543e2ef94a7
sdheda <shehreendheda@users.noreply.github.com>
2020-10-28 12:04:05 -0700

    updated with corrected content

--- logsintrov2/collectlogs2/foreground.sh
+++ logsintrov2/collectlogs2/foreground.sh
@@ -1,23 +1,11 @@
-# docker pull datadog/agent:6.11.1
-# docker pull redis:5.0.5
-# docker pull nginx:1.16.0
-cat > apikey.env <<EOL
----
-DD_API_KEY=${DD_API_KEY}
-EOL
+#!/bin/bash
 
-git clone https://github.com/DataDog/TrainingEnvironment.git
-cp -r TrainingEnvironment/logsintro/* .
-rm -rf TrainingEnvironment
-sed -i 's|vagrant|var/log|g' /root/main.py
-sed -i "s|write_text_log('.|write_text_log('/var/log|g" /root/main.py
-sed -i "s|write_json_log('.|write_json_log('/var/log|g" /root/main.py
-
-rm install.sh
-rm provision.sh
-rm Vagrantfile
-rm docker-compose.yml
-python main.py &
-echo "ready"
-
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=postgres
 clear
+while [ ! -f /root/app-files/docker-compose-files/docker-compose-fixed-instrumented.yml ]; do sleep 2; done
+cd /root/app-files/docker-compose-files
+clear
+docker-compose -f docker-compose-fixed-instrumented.yml up -d
+clear
+prep-environment
