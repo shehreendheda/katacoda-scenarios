69fd45c2ef7d252a5f1c4637f925669039de45bd
sdheda <shehreendheda@users.noreply.github.com>
2020-10-29 08:46:12 -0700

    corrected filename in fg, step titles in index, updated step1 instructions

--- logsintrov2/collectlogsapp/foreground.sh
+++ logsintrov2/collectlogsapp/foreground.sh
@@ -5,7 +5,7 @@ export POSTGRES_PASSWORD=postgres
 clear
 while [ ! -f /root/app-files/docker-compose-files/docker-compose-fixed-instrumented.yml ]; do sleep 2; done
 cd /root/app-files
-mv /root/docker-compose-no-instrumentation.yml /root/app-files/docker-compose-files/docker-compose-no-instrumentation.yml
+mv /root/docker-compose-no-logs.yml /root/app-files/docker-compose-files/docker-compose-no-logs.yml
 #mv docker-compose-broken-no-apm-instrumentation.yml docker-compose-broken-no-instrumentation.yml
 #rm docker-compose-broken-no-apm-instrumentation.yml
 #sed -i 's/ads-service/advertisements-service/' docker-compose-fixed-instrumented.yml
