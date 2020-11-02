a3282dd786d47f16fe68bbed7ec38c18f026896a
sdheda <shehreendheda@users.noreply.github.com>
2020-11-02 14:25:04 -0800

    test env files in assets

--- logsparse/foreground.sh
+++ logsparse/foreground.sh
@@ -6,14 +6,18 @@ cat > apikey.env <<EOL
 DD_API_KEY=${DD_API_KEY}
 EOL
 
-git clone https://github.com/DataDog/TrainingEnvironment.git
-cp -r TrainingEnvironment/logsparsing/* .
-rm -rf TrainingEnvironment
+#git clone https://github.com/DataDog/TrainingEnvironment.git
+#cp -r TrainingEnvironment/logsparsing/* .
+#rm -rf TrainingEnvironment
 
-sed -i 's|- DD_HOSTNAME=datadog|- DD_API_KEY\n\t- DD_HOSTNAME=datadog|' docker-compose.yml
+#sed -i 's|- DD_HOSTNAME=datadog|- DD_API_KEY\n\t- DD_HOSTNAME=datadog|' docker-compose.yml
 
-sed -i 's|datadog/agent:latest|datadog/agent:6.11.1|' docker-compose.yml
-sed -i 's|redis:latest|redis:5.0.5|' docker-compose.yml
-sed -i 's|nginx:latest|nginx:1.16.0|' docker-compose.yml
+#sed -i 's|datadog/agent:latest|datadog/agent:6.11.1|' docker-compose.yml
+#sed -i 's|redis:latest|redis:5.0.5|' docker-compose.yml
+#sed -i 's|nginx:latest|nginx:1.16.0|' docker-compose.yml
+
+while [ ! -f /root/app-files/docker-compose.yml ]; do sleep 1; done
+
+cd /root/lab-files
 
 docker-compose up
