376a8c30391fedcaf9488512648d23194023ee5b
sdheda <shehreendheda@users.noreply.github.com>
2020-11-03 14:49:58 -0800

    corrected cd before sed

--- slov2/slopractice/foreground.sh
+++ slov2/slopractice/foreground.sh
@@ -4,14 +4,15 @@ export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
 clear
 while [ ! -f /root/app-files/docker-compose-files/docker-compose-fixed-instrumented.yml ]; do sleep 2; done
-cd /root/app-files/docker-compose-files
+cd /root/app-files/
 #sed -i 's/.ruby-shop./ENV["DD_ENV"]/' ./store-frontend-instrumented-fixed/api/config/initializers/datadog.rb
 #sed -i 's/.ruby-shop./ENV["DD_ENV"]/' ./store-frontend-instrumented-fixed/config/initializers/datadog.rb
 #sed -i 's/.ruby-shop./ENV["DD_ENV"]/' ./store-frontend-instrumented-fixed/frontend/config/initializers/datadog.rb
-#sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service/requirements.txt
-#sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service-fixed/requirements.txt
+sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service/requirements.txt
+sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service-fixed/requirements.txt
 #mv /root/frontend-docker-entrypoint.sh ./store-frontend-instrumented-fixed/docker-entrypoint.sh
 clear
+cd /root/app-files/docker-compose-files
 docker-compose -f docker-compose-fixed-instrumented.yml up -d
 clear
 prep-environment
