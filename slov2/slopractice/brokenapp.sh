988c0d84c6ff7a821845d9f12cde1e2d192fc242
sdheda <shehreendheda@users.noreply.github.com>
2020-11-03 15:22:25 -0800

    updated for base-v1

--- slov2/slopractice/brokenapp.sh
+++ slov2/slopractice/brokenapp.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 clear
-cd /root/app-files/docker-compose-files
 docker-compose -f docker-compose-fixed-instrumented.yml stop
-clear
+docker-compose -f docker-compose-fixed-instrumented.yml rm -f
 docker-compose -f docker-compose-broken-instrumented.yml up -d
 clear
 prep-environment
