c63d605ba289f029c6f60066440be7f910e902da
sdheda <shehreendheda@users.noreply.github.com>
2020-11-03 14:41:54 -0800

    using base-v1, updating ddtrace

--- slov2/slopractice/foreground.sh
+++ slov2/slopractice/foreground.sh
@@ -5,6 +5,8 @@ export POSTGRES_PASSWORD=postgres
 clear
 while [ ! -f /root/app-files/docker-compose-files/docker-compose-fixed-instrumented.yml ]; do sleep 2; done
 cd /root/app-files/docker-compose-files
+sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service/requirements.txt
+sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service-fixed/requirements.txt
 clear
 docker-compose -f docker-compose-fixed-instrumented.yml up -d
 clear
