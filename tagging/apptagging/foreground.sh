ae3034804de15ed3ba97c6f1a0ed204dba47e7f7
sdheda <shehreendheda@users.noreply.github.com>
2020-11-09 10:58:55 -0800

    updated with base-v1

--- tagging/apptagging/foreground.sh
+++ tagging/apptagging/foreground.sh
@@ -2,10 +2,7 @@
 
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
-clear
-while [ ! -f /root/app-files/docker-compose-files/docker-compose-fixed-instrumented.yml ]; do sleep 2; done
-cd /root/app-files/docker-compose-files
-clear
+cd /ecommworkshop/docker-compose-files
 docker-compose -f docker-compose-fixed-instrumented.yml up -d
 clear
 prep-environment
