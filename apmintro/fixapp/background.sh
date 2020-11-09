095306bace9997a8e4f7943a5a9e5b06111d573a
sdheda <shehreendheda@users.noreply.github.com>
2020-11-09 11:49:51 -0800

    updated to base-v1

--- apmintro/fixapp/background.sh
+++ apmintro/fixapp/background.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-mkdir /root/app-files
-git clone https://github.com/DataDog/ecommerce-workshop /root/app-files
-cd /root/app-files
-docker-compose pull
-
-./gor --input-file-loop --input-file requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
+/ecommworkshop/gor --input-file-loop --input-file /ecommworkshop/requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
