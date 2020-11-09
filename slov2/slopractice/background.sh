be22394306cde610bacac6f8058c8605d6f8fc5f
sdheda <shehreendheda@users.noreply.github.com>
2020-11-09 09:30:27 -0800

    updated correct filepath for request_0.gor

--- slov2/slopractice/background.sh
+++ slov2/slopractice/background.sh
@@ -9,4 +9,4 @@
 
 #docker-compose pull
 
-/ecommworkshop/gor --input-file-loop --input-file requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
+/ecommworkshop/gor --input-file-loop --input-file /ecommworkshop/requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
