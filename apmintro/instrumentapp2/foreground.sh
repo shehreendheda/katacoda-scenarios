bed9e2b216e41ac8223a2f2174a3de2339bf3436
sdheda <shehreendheda@users.noreply.github.com>
2020-11-09 11:25:25 -0800

    updated to base-v1

--- apmintro/instrumentapp2/foreground.sh
+++ apmintro/instrumentapp2/foreground.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
+
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
-clear
-while [ ! -f /root/app-files/docker-compose-files/docker-compose-broken-no-apm-instrumentation.yml ]; do sleep 2; done
-cd /root/app-files/docker-compose-files
-clear
+cd /ecommworkshop/docker-compose-files
 docker-compose -f docker-compose-broken-no-apm-instrumentation.yml up -d
 clear
 prep-environment
