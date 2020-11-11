4f3f66a20f35c25028eac65352560513f1f17d2e
sdheda <shehreendheda@users.noreply.github.com>
2020-11-11 10:42:53 -0800

    adding env tag

--- logsprocess/logspipeline/foreground.sh
+++ logsprocess/logspipeline/foreground.sh
@@ -1,12 +1,11 @@
 #! /usr/bin/env
 
-wait $DD_API_Key
-
 docker run -d --name datadog-agent \
     -e DD_API_KEY=${DD_API_KEY} \
     -e DD_LOGS_ENABLED=true \
     -e DD_LOGS_CONFIG_CONTAINER_COLLECT_ALL=true \
     -e DD_CONTAINER_EXCLUDE_LOGS="image:datadog/agent" \
+    -e DD_ENV="logspipeline" \
     -v /var/run/docker.sock:/var/run/docker.sock:ro \
     -v /proc/:/host/proc/:ro \
     -v /opt/datadog-agent/run:/opt/datadog-agent/run:rw \
@@ -15,4 +14,3 @@ docker run -d --name datadog-agent \
 
 docker run --name flog -d -it --rm mingrammer/flog -f apache_combined -l -n 100000 -d 0.2
 
-clear
