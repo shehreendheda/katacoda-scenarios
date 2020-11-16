8086b8ddae89d26cd0badf442d0133f2266ccd8b
sdheda <shehreendheda@users.noreply.github.com>
2020-11-16 10:15:00 -0800

    removed until loop, corrected while loop

--- logsprocess/logsparsing/background.sh
+++ logsprocess/logsparsing/background.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-until $(curl -o /dev/null -s -I -f http://localhost:8080)
-do
-    sleep 5
-done
-
 while true
 do
-	`curl -X GET 'http://localhost:8080/think/?subject=technology'`
+	curl -X GET 'http://localhost:8080/think/?subject=technology'
     sleep 5
-    `curl -X GET 'http://localhost:8080/think/?subject=religion'`
+    curl -X GET 'http://localhost:8080/think/?subject=religion'
     sleep 5
-    `curl -X GET 'http://localhost:8080/think/?subject=war'`
+    curl -X GET 'http://localhost:8080/think/?subject=war'
     sleep 5
-    `curl -X GET 'http://localhost:8080/think/?subject=work'`
+    curl -X GET 'http://localhost:8080/think/?subject=work'
     sleep 5
-    `curl -X GET 'http://localhost:8080/think/?subject=music'`
+    curl -X GET 'http://localhost:8080/think/?subject=music'
     sleep 5
 done
