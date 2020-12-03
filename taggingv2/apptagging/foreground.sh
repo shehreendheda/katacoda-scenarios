25059f7f167c8794e03f5b3b6c350cca8a2b97b9
sdheda <shehreendheda@users.noreply.github.com>
2020-12-03 11:38:25 -0800

    corrected fg, corrected broken yml, switched service version numbers

--- taggingv2/apptagging/foreground.sh
+++ taggingv2/apptagging/foreground.sh
@@ -3,13 +3,6 @@
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
 cd /ecommworkshop
-
-while [ ! -f /ecommworkshop/docker-compose-broken.yml ]; do
-    printf '.'
-    sleep 2; 
-done
-printf '\n'
-
 docker-compose -f docker-compose-broken.yml up -d
 clear
 prep-environment
