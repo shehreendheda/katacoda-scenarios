72d774ff00f3fbf00bcbbff0a6c27b21eb237df0
sdheda <shehreendheda@users.noreply.github.com>
2020-12-03 13:25:38 -0800

    added sed ddtrace update

--- taggingv2/apptagging/foreground.sh
+++ taggingv2/apptagging/foreground.sh
@@ -4,6 +4,11 @@ export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
 while [ ! -f /ecommworkshop/docker-compose-broken.yml ]; do sleep 2; done
 cd /ecommworkshop
+
+sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service/requirements.txt
+sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service-fixed/requirements.txt
+sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service/requirements.txt
+
 docker-compose -f docker-compose-broken.yml up -d
 clear
 prep-environment
