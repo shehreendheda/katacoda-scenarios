45aaf26f65c6fafb87d5e941c720a8bad6746fb5
sdheda <shehreendheda@users.noreply.github.com>
2020-12-03 13:03:21 -0800

    added wait for docker-compose

--- taggingv2/apptagging/foreground.sh
+++ taggingv2/apptagging/foreground.sh
@@ -2,6 +2,7 @@
 
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
+while [ ! -f /ecommworkshop/docker-compose-broken.yml ]; do sleep 2; done
 cd /ecommworkshop
 docker-compose -f docker-compose-broken.yml up -d
 clear
