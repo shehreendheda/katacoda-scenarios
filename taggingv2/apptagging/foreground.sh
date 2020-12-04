ad36bf301a00e94cdb0dbfe184c5fee818d4c3e3
sdheda <shehreendheda@users.noreply.github.com>
2020-12-04 13:51:30 -0800

    added frontend-docker-entrypoint.sh to update ddtrace for versioning

--- taggingv2/apptagging/foreground.sh
+++ taggingv2/apptagging/foreground.sh
@@ -8,6 +8,8 @@ cd /ecommworkshop
 sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service/requirements.txt
 sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service-fixed/requirements.txt
 sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service/requirements.txt
+cp /root/frontend-docker-entrypoint.sh ./store-frontend-instrumented-fixed/docker-entrypoint.sh
+cp /root/frontend-docker-entrypoint.sh ./store-frontend-broken-instrumented/docker-entrypoint.sh
 
 docker-compose -f docker-compose-fixed.yml up -d
 clear
