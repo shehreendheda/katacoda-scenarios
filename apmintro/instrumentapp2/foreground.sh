53d18f7080e3b70d97be58591d642cede447e1ad
sdheda <shehreendheda@users.noreply.github.com>
2020-12-11 09:25:41 -0800

    removing clears

--- apmintro/instrumentapp2/foreground.sh
+++ apmintro/instrumentapp2/foreground.sh
@@ -5,10 +5,9 @@ export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
 cd /ecommworkshop/docker-compose-files
 docker-compose -f docker-compose-broken-no-apm-instrumentation.yml up -d
-clear
+
 
 #envready
 #statusupdate complete
-clear
 
 prepenvironment
