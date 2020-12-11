3f676c53b41d5c69ac0e69a7bb6cebc125a20386
sdheda <shehreendheda@users.noreply.github.com>
2020-12-11 09:33:00 -0800

    moved envready and statusupdate to bg

--- apmintro/fixapp/background.sh
+++ apmintro/fixapp/background.sh
@@ -2,4 +2,8 @@
 
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 
+envready
+
 /ecommworkshop/gor --input-file-loop --input-file /ecommworkshop/requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
+
+statusupdate complete
