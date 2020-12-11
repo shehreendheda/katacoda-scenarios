f0e94ef8e8d84d6c0629dccbb6cf154b68da0aef
sdheda <shehreendheda@users.noreply.github.com>
2020-12-11 10:42:56 -0800

    moved envready and statusupdate back to fg

--- apmintro/fixapp/foreground.sh
+++ apmintro/fixapp/foreground.sh
@@ -4,5 +4,10 @@ export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
 cd /ecommworkshop/docker-compose-files
 docker-compose -f docker-compose-broken-instrumented.yml up -d
+clear
+
+envready
+statusupdate complete
+clear
 
 prepenvironment
