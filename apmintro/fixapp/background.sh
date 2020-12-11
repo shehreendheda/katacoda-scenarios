5c48f14848a560a7c004ce3e6c4b52b7bea9b663
sdheda <shehreendheda@users.noreply.github.com>
2020-12-11 09:42:58 -0800

    moved statusupdate before traffic command

--- apmintro/fixapp/background.sh
+++ apmintro/fixapp/background.sh
@@ -3,7 +3,6 @@
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 
 envready
+statusupdate complete
 
 /ecommworkshop/gor --input-file-loop --input-file /ecommworkshop/requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
-
-statusupdate complete
