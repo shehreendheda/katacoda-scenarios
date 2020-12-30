dc18f9e19ede934fa3fe1f53da23363592569e58
sdheda <shehreendheda@users.noreply.github.com>
2020-12-30 12:07:18 -0800

    added traffic command

--- logsintrov2/managelogs2/background.sh
+++ logsintrov2/managelogs2/background.sh
@@ -9,3 +9,5 @@ git fetch
 git checkout e400e3fc ./ads-service-fixed
 
 statusupdate files
+
+/ecommworkshop/gor --input-file-loop --input-file /ecommworkshop/requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
