22929df4b6cbaa512b9962c40f240e48c8213832
sdheda <shehreendheda@users.noreply.github.com>
2020-12-30 10:57:49 -0800

    updated files for versioned fixed app

--- logsintrov2/managelogs2/background.sh
+++ logsintrov2/managelogs2/background.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
+
+mv /root/docker-compose-fixed.yml /ecommworkshop/
+
+cd /ecommworkshop
+git fetch
+git checkout e400e3fc ./ads-service-fixed
+
+statusupdate files
--- logsintrov2/managelogs2/foreground.sh
+++ logsintrov2/managelogs2/foreground.sh
@@ -2,12 +2,19 @@
 
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
-cd /ecommworkshop/docker-compose-files
-docker-compose -f docker-compose-fixed-instrumented.yml up -d
+
+statuscheck files
+
+cd /ecommworkshop
+
+sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service-fixed/requirements.txt
+sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service-fixed/requirements.txt
+cp /root/frontend-docker-entrypoint.sh ./store-frontend-instrumented-fixed/docker-entrypoint.sh
+
+docker-compose -f docker-compose-fixed.yml up -d
 clear
 
 envready
 statusupdate complete
-clear
 
 prepenvironment
