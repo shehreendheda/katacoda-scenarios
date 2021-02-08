73f2524537048243a55bd65aa949908d5c11ce5f
sdheda <shehreendheda@users.noreply.github.com>
2021-02-08 11:16:18 -0800

    updated fg, bg, and index to use simple base-v1 setup

--- logsintrov2/processlogs/foreground.sh
+++ logsintrov2/processlogs/foreground.sh
@@ -2,20 +2,10 @@
 
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
-
-statuscheck files
-
-cd /ecommworkshop
-
-sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service/requirements.txt
-sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service-fixed/requirements.txt
-sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service/requirements.txt
-sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service-fixed/requirements.txt
-cp /root/frontend-docker-entrypoint.sh ./store-frontend-instrumented-fixed/docker-entrypoint.sh
-cp /root/frontend-docker-entrypoint.sh ./store-frontend-broken-instrumented/docker-entrypoint.sh
-
-docker-compose -f docker-compose-fixed.yml up -d
+cd /ecommworkshop/docker-compose-files
+docker-compose -f docker-compose-broken-instrumented.yml up -d
 clear
+
 envready
 statusupdate complete
 clear
