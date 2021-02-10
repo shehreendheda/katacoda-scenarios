eec30ce592cbad8d64e017063abf88c9938ad626
sdheda <shehreendheda@users.noreply.github.com>
2021-02-09 16:01:19 -0800

    using fixed app

--- logsintrov2/processlogs/foreground.sh
+++ logsintrov2/processlogs/foreground.sh
@@ -3,7 +3,7 @@
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
 cd /ecommworkshop/deploy/docker-compose
-docker-compose -f docker-compose-broken-instrumented.yml up -d
+docker-compose -f docker-compose-fixed-instrumented.yml up -d
 clear
 
 envready
