d75758d11cca5928e540d21eb91636c13b66121d
sdheda <shehreendheda@users.noreply.github.com>
2021-02-16 09:11:35 -0800

    commented docker-compose up and env ready

--- logsintrov2/processlogs/foreground.sh
+++ logsintrov2/processlogs/foreground.sh
@@ -3,10 +3,10 @@
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
 cd /ecommworkshop/deploy/docker-compose
-docker-compose -f docker-compose-fixed-instrumented.yml up -d
+#docker-compose -f docker-compose-fixed-instrumented.yml up -d
 clear
 
-envready
+#envready
 statusupdate complete
 clear
 
