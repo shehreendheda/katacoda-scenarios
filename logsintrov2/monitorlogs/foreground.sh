6729cb6021db09650a85bb095ba2bee46eeac535
sdheda <shehreendheda@users.noreply.github.com>
2021-02-19 12:48:07 -0800

    updated to use new docker-compose and images, added step titles

--- logsintrov2/monitorlogs/foreground.sh
+++ logsintrov2/monitorlogs/foreground.sh
@@ -2,12 +2,25 @@
 
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
+
+statuscheck files
+
+cd /ecommworkshop
+sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service/requirements.txt
+sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service-fixed/requirements.txt
+sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service-fixed/requirements.txt
+sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service/requirements.txt
+cp /root/frontend-docker-entrypoint.sh ./store-frontend-instrumented-fixed/docker-entrypoint.sh
+cp /root/frontend-docker-entrypoint.sh ./store-frontend-broken-instrumented/docker-entrypoint.sh
+
 cd /ecommworkshop/deploy/docker-compose
-docker-compose -f docker-compose-fixed-instrumented.yml up -d
-clear
+docker-compose -f docker-compose-fixed.yml up -d
 
 envready
-statusupdate complete
-clear
 
-prepenvironment
+docker kill docker-compose_db_1
+
+statusupdate complete
+
+
+#prepenvironment
