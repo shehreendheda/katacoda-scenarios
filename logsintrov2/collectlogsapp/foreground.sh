237c204aa2e9c67ea2bed3e49fb504869c445ef1
sdheda <shehreendheda@users.noreply.github.com>
2021-02-26 09:41:09 -0800

    commenting lines for new frontend files

--- logsintrov2/collectlogsapp/foreground.sh
+++ logsintrov2/collectlogsapp/foreground.sh
@@ -7,8 +7,8 @@ statuscheck files
 
 cd /ecommworkshop
 #rm ./store-frontend-instrumented-fixed
-git fetch
-git checkout 3466aae ./store-frontend-instrumented-fixed
+#git fetch
+#git checkout 3466aae ./store-frontend-instrumented-fixed
 
 
 sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service/requirements.txt
