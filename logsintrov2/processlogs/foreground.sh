c385f7094a5c88956700f2d4b44399975217b0c3
sdheda <shehreendheda@users.noreply.github.com>
2021-03-01 09:53:54 -0800

    using update service images

--- logsintrov2/processlogs/foreground.sh
+++ logsintrov2/processlogs/foreground.sh
@@ -2,12 +2,28 @@
 
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
+
+statuscheck files
+
+cd /ecommworkshop
+rm ./store-frontend-instrumented-fixed
+git fetch
+git checkout 3466aae ./store-frontend-instrumented-fixed
+
+
+sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service/requirements.txt
+sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service-fixed/requirements.txt
+sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service-fixed/requirements.txt
+sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service/requirements.txt
+cp /root/frontend-docker-entrypoint.sh ./store-frontend-instrumented-fixed/docker-entrypoint.sh
+cp /root/frontend-docker-entrypoint.sh ./store-frontend-broken-instrumented/docker-entrypoint.sh
+
 cd /ecommworkshop/deploy/docker-compose
-docker-compose -f docker-compose-fixed-instrumented.yml up -d#
-clear
+docker-compose -f docker-compose-fixed.yml up -d
 
 envready
-statusupdate complete
-clear
 
-prepenvironment
+statusupdate complete
+
+
+#prepenvironment
