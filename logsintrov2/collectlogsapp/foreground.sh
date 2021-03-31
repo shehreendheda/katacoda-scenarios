4cfedcd02a1e7c6d97a43306d819f3d3cf49ed3d
sdheda <shehreendheda@users.noreply.github.com>
2021-03-31 08:02:05 -0700

    updated fg, bg to latest

--- logsintrov2/collectlogsapp/foreground.sh
+++ logsintrov2/collectlogsapp/foreground.sh
@@ -6,20 +6,26 @@ export POSTGRES_PASSWORD=postgres
 statuscheck files
 
 cd /ecommworkshop
-#rm ./store-frontend-instrumented-fixed
-#git fetch
-#git checkout 3466aae ./store-frontend-instrumented-fixed
+rm ./store-frontend-instrumented-fixed
+git branch -m master main
+git fetch origin
+git branch -u origin/main main
+git checkout 30e3b3f ./store-frontend-instrumented-fixed
+git checkout 30e3b3f ./store-frontend-broken-instrumented
 
 
 sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service/requirements.txt
 sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service-fixed/requirements.txt
 sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service-fixed/requirements.txt
 sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service/requirements.txt
-cp /root/frontend-docker-entrypoint.sh ./store-frontend-instrumented-fixed/docker-entrypoint.sh
-cp /root/frontend-docker-entrypoint.sh ./store-frontend-broken-instrumented/docker-entrypoint.sh
+#cp /root/frontend-docker-entrypoint.sh ./store-frontend-instrumented-fixed/docker-entrypoint.sh
+#cp /root/frontend-docker-entrypoint.sh ./store-frontend-broken-instrumented/docker-entrypoint.sh
 
 cd /ecommworkshop/deploy/docker-compose
-docker-compose -f docker-compose-no-logs.yml up -d
+
+#docker system prune --all --force
+
+docker-compose -f docker-compose-broken.yml up -d
 
 envready
 
