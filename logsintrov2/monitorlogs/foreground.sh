42fe671942179bb3aaa4585592bf695f2fdac90a
sdheda <shehreendheda@users.noreply.github.com>
2021-04-06 08:14:35 -0700

    updated to latest service images and commit

--- logsintrov2/monitorlogs/foreground.sh
+++ logsintrov2/monitorlogs/foreground.sh
@@ -8,16 +8,16 @@ statuscheck files
 cd /ecommworkshop
 rm ./store-frontend-instrumented-fixed
 git fetch
-git checkout 3466aae ./store-frontend-instrumented-fixed
-git checkout 3466aae ./store-frontend-broken-instrumented
+git checkout caadca9 ./store-frontend-instrumented-fixed
+git checkout caadca9 ./store-frontend-broken-instrumented
 
 
 sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service/requirements.txt
 sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service-fixed/requirements.txt
 sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service-fixed/requirements.txt
 sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service/requirements.txt
-cp /root/frontend-docker-entrypoint.sh ./store-frontend-instrumented-fixed/docker-entrypoint.sh
-cp /root/frontend-docker-entrypoint.sh ./store-frontend-broken-instrumented/docker-entrypoint.sh
+#cp /root/frontend-docker-entrypoint.sh ./store-frontend-instrumented-fixed/docker-entrypoint.sh
+#cp /root/frontend-docker-entrypoint.sh ./store-frontend-broken-instrumented/docker-entrypoint.sh
 
 cd /ecommworkshop/deploy/docker-compose
 docker-compose -f docker-compose-fixed.yml up -d
