2acdb3e75559a71172f69332d9b709c6cf51c109
sdheda <shehreendheda@users.noreply.github.com>
2021-07-09 10:16:20 -0700

    trying 2.0.0 release

--- rumcwv/background.sh
+++ rumcwv/background.sh
@@ -7,4 +7,4 @@ mv /root/docker-compose-broken.yml /ecommworkshop/deploy/docker-compose/
 mv /root/docker-compose-slowads.yml /ecommworkshop/deploy/docker-compose/
 statusupdate files
 
-/ecommworkshop/gor --input-file-loop --input-file "/ecommworkshop/requests_0.gor|300%" --output-http "http://localhost:3000" >> /dev/null 2>&1
+#/ecommworkshop/gor --input-file-loop --input-file "/ecommworkshop/requests_0.gor|300%" --output-http "http://localhost:3000" >> /dev/null 2>&1
--- rumcwv/foreground.sh
+++ rumcwv/foreground.sh
@@ -8,15 +8,25 @@ statuscheck files
 cd /ecommworkshop
 rm -r ./store-frontend-instrumented-fixed
 rm -r ./store-frontend-broken-instrumented
+rm -r ./ads-service
+rm -r ./ads-service-fixed
+rm -r ./discounts-service
+rm -r ./discounts-service-fixed
+
 git fetch
-git checkout 11e4a4b ./store-frontend-instrumented-fixed
-git checkout 11e4a4b ./store-frontend-broken-instrumented
+git checkout ccf7325 ./store-frontend-instrumented-fixed
+git checkout ccf7325 ./store-frontend-broken-instrumented
+git checkout ccf7325 ./ads-service
+git checkout ccf7325 ./ads-service-fixed
+git checkout ccf7325 ./discounts-service
+git checkout ccf7325 ./discounts-service-fixed
+git checkout ccf7325 ./traffic-replay
 
 
-sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service/requirements.txt
-sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service-fixed/requirements.txt
-sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service-fixed/requirements.txt
-sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service/requirements.txt
+#sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service/requirements.txt
+##ed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service-fixed/requirements.txt
+#sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service-fixed/requirements.txt
+#sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service/requirements.txt
 #cp /root/frontend-docker-entrypoint.sh ./store-frontend-instrumented-fixed/docker-entrypoint.sh
 #cp /root/frontend-docker-entrypoint.sh ./store-frontend-broken-instrumented/docker-entrypoint.sh
 
