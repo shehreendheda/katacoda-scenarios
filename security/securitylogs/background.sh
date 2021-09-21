e3dfa6583ee1dcd400a4f404cad71972436178a5
sdheda <shehreendheda@users.noreply.github.com>
2021-09-21 11:19:45 -0700

    using /root/lab as uieditorpath

--- security/securitylogs/background.sh
+++ security/securitylogs/background.sh
@@ -2,10 +2,12 @@
 
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true%7Cbash
 
-mv /root/docker-compose.yml /ecommworkshop/deploy/docker-compose/
+mv /root/docker-compose.yml /root/lab
 
 cd /ecommworkshop
 git fetch
 git checkout tags/2.0.0
+
+docker-compose -f /root/lab/docker-compose.yml pull
 
 statusupdate environment
